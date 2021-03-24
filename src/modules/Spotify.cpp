3de7bfd246ab2a6b1ee43627158e25f9ed01f101
Anthony Birkett <antbir@gmail.com>
2021-03-24 13:44:11 +0000

    Add Spotify track metadata.

--- src/modules/Spotify.cpp
+++ src/modules/Spotify.cpp
@@ -10,7 +10,60 @@ Spotify::Spotify()
 
 std::string Spotify::getOutput()
 {
-    auto meta = spotify->Metadata();
+    std::string status = spotify->PlaybackStatus();
+    std::string artist;
+    std::string album;
+    std::string title;
+    std::string output;
 
-    return spotify->PlaybackStatus();
+    bool isPlaying = status == "Playing";
+    bool isPaused = status == "Paused";
+    bool isStopped = status == "Stopped";
+
+    for (auto const& item : spotify->Metadata())
+    {
+        if (item.first == "xesam:artist") {
+            artist = item.second.reader().recurse().get_string();
+        }
+
+        if (item.first == "xesam:album") {
+            album = item.second.reader().get_string();
+        }
+
+        if (item.first == "xesam:title") {
+            title = item.second.reader().get_string();
+        }
+    }
+
+    output.append("\uF1BC "); // Spotify icon.
+
+    if (isStopped) {
+        output.append("\uF04D "); // Stopped icon.
+
+        return output;
+    }
+
+    if (isPlaying) {
+        output.append("\uF04B "); // Play icon.
+    }
+
+    if (isPaused) {
+        output.append("\uF28B "); // Pause icon.
+    }
+
+    if (artist.length() > 0) {
+        output.append(artist);
+    }
+
+    if (album.length() > 0) {
+        output.append(" - ");
+        output.append(album);
+    }
+
+    if (title.length() > 0) {
+        output.append(" - ");
+        output.append(title);
+    }
+
+    return output;
 }
